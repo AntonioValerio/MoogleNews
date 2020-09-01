7eaa01039b9de18b5f3517ac3e7e50ea741ee03b
antonio valerio <antoni_valerio@hotmail.com>
2020-09-01 22:17:29 +0100

    Fixed services

--- src/service/article-service.js
+++ src/service/article-service.js
@@ -1,31 +1,48 @@
-const articles = require('./article.json');
+const db = require('../configs/sequelize.js').getDB();
 
 exports.getArticles = () =>{
     return new Promise((resolve,reject) =>{
-        resolve(articles);
+        db.articles
+        .findAll({attributes :['_id','title','game','author']})
+        .then( articles => resolve(articles))
+        .catch(err => reject(err));
     });
 };
 
 exports.getArticle = id =>{
     return new Promise((resolve,reject)=>{
-        resolve(articles.find(article=> article._id===id));
+        //resolve(articles.find(article=> article._id===id));
+        db.articles
+        .findByPk(id)
+        .then(article => resolve(article))
+        .catch(err = reject(err));
     });
 };
 
 exports.addArticle = article =>{
     return new Promise((resolve,reject)=>{
-        resolve({inserted:1 });
+       // resolve({inserted:1 });
+       db.articles
+       .create(body)
+       .then(article => resolve({inserted: 1, _id: article._id}))
+       .catch(err => reject(err));
     });
 };
 
 exports.updateArticle = (id,article) =>{
     return new Promise((resolve,reject)=>{
-        resolve({updated:1});
+       // resolve({updated:1});
+       db.articles
+       .update(id,body)
+       .then(article => resolve({updated:1 , _id: article._id}))
     });
 };
 
 exports.deleteArticle = id =>{
     return new Promise((resolve,reject)=>{
-        resolve ({deleted:1});
+        //resolve ({deleted:1});
+        db.articles
+        .delete(id)
+        .then(article => resolve({deleted :1}))
     });
 };
